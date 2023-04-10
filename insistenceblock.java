6c0d50c7b5830fc38b3fb42532c1da8ee44f167a
Batwan01 <wldhks1120@naver.com>
2023-04-10 21:36:31 +0900

    insistence block

---
+++ insistenceblock.java
@@ -0,0 +1,20 @@
+class Product {
+	static int count = 0;
+	int serialNo;
+
+	{ ++count; serialNo = count; }
+
+	public Product() {}
+}
+class ProductTest {
+	public static void main(String args[]) {
+		Product p1 = new Product();
+		Product p2 = new Product();
+		Product p3 = new Product();
+
+		System.out.println("p1(serial no) " + p1.serialNo);
+		System.out.println("p2(serial no) " + p2.serialNo);
+		System.out.println("p3(serial no) " + p3.serialNo);
+		System.out.println("product count : "+Product.count);
+	}
+}
